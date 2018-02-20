9259a9c428cfc4f5d8ac53c1c1ce52a79c7cefdc
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-02-20 09:41:16 +0100

    Completed a little demo script

--- lib/midi_visualizer/effect/adsr.rb
+++ lib/midi_visualizer/effect/adsr.rb
@@ -11,9 +11,10 @@ module MIDIVisualizer
     # TODO: Investigate the effects of chainging the params during operation.
     class ADSR
       DEFAULT_PARAMS = {
-        attack_slope: 1.0,
-        decay_value: 0.8,
-        decay_slope: 1.0,
+        attack_slope:  1.0,
+        attack_value:  1.0,
+        decay_value:   0.8, # Percentage of the attack_value
+        decay_slope:   1.0,
         release_slope: 1.0
       }.freeze
 
@@ -46,6 +47,10 @@ module MIDIVisualizer
         end
       end
 
+      def done?
+        @state == :done
+      end
+
       private
 
       # Returns either a symbol if the effect should transition to another state
@@ -53,7 +58,7 @@ module MIDIVisualizer
       def attack_state(t, params)
         slope = params[:release_slope]
         dt    = t - @start_t
-        return :decay if dt > (1.0 - @start_value) / slope
+        return :decay if dt > (params[:attack_value] - @start_value) / slope
 
         @start_value + dt * slope
       end
@@ -62,16 +67,18 @@ module MIDIVisualizer
       # or a numeric value.
       def decay_state(t, params)
         slope = params[:attack_slope]
-        dt    = t - @start_t - (1.0 - @start_value) / slope
+        top   = params[:attack_value]
+        decay = top * params[:decay_value]
+        dt    = t - @start_t - (top - @start_value) / slope
 
-        return :sustain if dt > (1.0 - params[:decay_value]) / slope
+        return :sustain if dt > (top - decay) / slope
 
-        1.0 - dt * slope
+        top - dt * slope
       end
 
       # Returns a constant value.
       def sustain_state(_, params)
-        params[:decay_value]
+        params[:decay_value] * params[:attack_value]
       end
 
       # Returns either a symbol if the effect should transition to another state
